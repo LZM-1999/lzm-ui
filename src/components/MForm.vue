1de3d389a8bc10bfb9fdf5ae47d6ca4ab941edcb
李忠美 <1204853301@qq.com>
2023-03-01 16:09:54 +0800

    fix

--- src/components/MForm.vue
+++ src/components/MForm.vue
@@ -1,28 +1,27 @@
 <template>
-    <el-form :inline='true' class="demo-form-inline" :model="form" ref="form">
-        <template v-for="item in formItemProp">
-            <el-form-item :key="item.prop" :label="item.label" v-if="item.prop !== 'handle'">
-                <slot :name="item.label" :slot-scope="[formItemProp, form, item]">
-                    <el-input v-if="item.itemProp.type=='input'" v-model="form[item.prop]" :placeholder="item.itemProp.placeholder"></el-input>
-                    <el-select v-if="item.itemProp.type=='select'" v-model="form[item.prop]" :placeholder="item.itemProp.placeholder">
-                        <el-option v-for="option in item.itemProp.selectProp" :label=" option.label" :value="option.value" :key="option.id || option.value || option.label"/>
-                    </el-select>
-                    <el-date-picker v-if="item.itemProp.type=='time'" v-model="form[item.prop]" :placeholder="item.itemProp.placeholder" v-bind="item.itemProp.timeParam"/>
-                </slot>
-            </el-form-item>
-            <slot  v-else-if="item.prop=='handle'" :name="item.label" :slot-scope="{ formItemProp , form}">
-                <el-button 
-                :type="item.handleProp.type == 'reset' ? 'danger' : 'primary' " 
-                :key="item.prop" 
-                :icon="item.handleProp.type == 'search' ? 'el-icon-search' : ''"
-                @click="handleEvent(item.handleProp.type)"
-                >
-                {{ item.label }}
-                </el-button>
-            </slot>
-        </template>
-        {{ form }}
-    </el-form>
+  <el-form :inline='true' class="demo-form-inline" :model="form" ref="form">
+      <template v-for="item in formItemProp">
+          <el-form-item :key="item.prop" :label="item.label" v-if="item.prop !== 'handle'">
+              <slot :name="item.label" :slot-scope="{formItemProp, form, target:item}">
+                  <el-input v-if="item.itemProp.type=='input'" v-model="form[item.prop]" :placeholder="item.itemProp.placeholder"></el-input>
+                  <el-select v-if="item.itemProp.type=='select'" v-model="form[item.prop]" :placeholder="item.itemProp.placeholder">
+                      <el-option v-for="option in item.itemProp.selectProp" :label=" option.label" :value="option.value" :key="option.id || option.value || option.label"/>
+                  </el-select>
+                  <el-date-picker v-if="item.itemProp.type=='time'" v-model="form[item.prop]" :placeholder="item.itemProp.placeholder" v-bind="item.itemProp.timeParam"/>
+              </slot>
+          </el-form-item>
+          <slot  v-else-if="item.prop=='handle'" :name="item.label" :slot-scope="{ formItemProp , form}">
+              <el-button 
+              :type="item.handleProp.type == 'reset' ? 'danger' : 'primary' " 
+              :key="item.prop" 
+              :icon="item.handleProp.type == 'search' ? 'el-icon-search' : ''"
+              @click="handleEvent(item.handleProp.type)"
+              >
+              {{ item.label }}
+              </el-button>
+          </slot>
+      </template>
+  </el-form>
 </template>
 
 <script>
